577f38df4740c2dd584c3322404ff56fbe4870f1
ShinoChan0 <131277759+Ryosei1009@users.noreply.github.com>
2024-02-25 22:07:40 +0900

    HeaderRight削除

--- frontend/src/components/_util/Header.js
+++ frontend/src/components/_util/Header.js
@@ -1,7 +1,18 @@
-import React from 'react'
-import HeaderRight from './HeaderRight';
+import React, { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { getAccountData } from "../../utils/AccountUtil";
+import Loading from "./Loading";
 
 const Header = () => {
+    const { isAuthenticated, isLoading, user } = useAuth0();
+    const [eachAccount, setEachAccount] = useState({});
+    useEffect(() => {
+        getAccountData(user, setEachAccount);
+    }, [user]);
+
+    if (isLoading) {
+        return <Loading />;
+    }
 
     return (
         <>
@@ -28,7 +39,13 @@ const Header = () => {
                     </ul>
                 </nav>
                 <div className="flex items-center">
-                    <HeaderRight />
+                    {isAuthenticated && (
+                        <div>
+                            <a href="/profile/" >
+                                <img className="w-12 rounded-full" src={eachAccount.icon_name === undefined || eachAccount.icon_name === "" ? `/images/accounts/default.jpeg` : `${process.env.REACT_APP_IMAGE_DOMAIN}/${eachAccount.icon_name}`} alt="" />
+                            </a>
+                        </div>
+                    )}
                 </div>
             </header>
         </>
