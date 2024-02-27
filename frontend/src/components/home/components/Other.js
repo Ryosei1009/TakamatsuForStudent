d1e643cae7a5520fbbf67f7c575c5a0b87e2aa88
ShinoChan0 <131277759+Ryosei1009@users.noreply.github.com>
2024-02-27 20:06:33 +0900

    JsonファイルでOthersカテゴリを管理できるように

--- frontend/src/components/home/components/Other.js
+++ frontend/src/components/home/components/Other.js
@@ -1,67 +1,33 @@
 import React from 'react'
+import othersjson from './Others.json'
 
 const SelfIntroduction = () => {
   return (
     <div className="mx-72 max-2xl:mx-36 max-xl:mx-18 max-lg:mx-8 max-sm:mx-4 my-16">
-      <a href='/selfintroduction'>
-        <div class="bg-violet-200 rounded-3xl px-12 py-8 mb-16 hover:opacity-70">
-          <div class="text-5xl font-bold">
-            自己紹介
-          </div>
-          <div class="flex max-md:flex-col items-center justify-around mt-8">
-            <div class="max-md:w-full w-1/2">
-              <img src="/images/home/comingsoon.png" alt="" class="w-full rounded-lg" />
+      {othersjson.slice().map((item) => (
+        <a href={item.url} key={item.id}>
+          <div class="bg-violet-200 rounded-3xl px-12 py-8 mb-16 hover:opacity-70">
+            <div class="text-5xl font-bold">
+              {item.title}
             </div>
-            <div class="max-md:w-full w-1/2 font-bold text-3xl max-xl:text-2xl max-lg:text-xl max-md:text-lg ml-6 max-md:ml-0 border-l-4 max-md:border-l-0 border-black pl-6 max-md:px-3 max-md:mt-3 max-md:border-t-4 max-md:pt-2">
-              現在我々は、母なる星『地球』に住んでいますが、人類はどの時代でも宙を眺め観測してきました。
-              <br />
-              ですが我々が住む青い星地球は、さまざまな問題に直面している。
-              <br /><br />
-              母なる星、地球から独立し新たなるUniverse (宇宙)へ飛び出しましょう!
+            <div class="flex max-md:flex-col items-center justify-around mt-8">
+              <div class="max-md:w-full w-1/2">
+                <img src={`/images/home/${item.image}`} alt="" class="w-full rounded-lg" />
+              </div>
+              <div class="max-md:w-full w-1/2 font-bold text-3xl max-xl:text-2xl max-lg:text-xl max-md:text-lg ml-6 max-md:ml-0 border-l-4 max-md:border-l-0 border-black pl-6 max-md:px-3 max-md:mt-3 max-md:border-t-4 max-md:pt-2">
+                {item.text1}
+                <br />
+                {item.text2}
+                <br />
+                {item.text3}
+                <br />
+                {item.text4}
+              </div>
             </div>
           </div>
-        </div>
-      </a>
+        </a>
+      ))}
 
-      <a href='/event'>
-        <div class="bg-violet-200 rounded-3xl px-12 py-8 mb-16 hover:opacity-70">
-          <div class="text-5xl font-bold">
-            イベントカレンダー
-          </div>
-          <div class="flex max-md:flex-col items-center justify-around mt-8">
-            <div class="max-md:w-full w-1/2">
-              <img src="/images/home/comingsoon.png" alt="" class="w-full rounded-lg" />
-            </div>
-            <div class="max-md:w-full w-1/2 font-bold text-3xl max-xl:text-2xl max-lg:text-xl max-md:text-lg ml-6 max-md:ml-0 border-l-4 max-md:border-l-0 border-black pl-6 max-md:px-3 max-md:mt-3 max-md:border-t-4 max-md:pt-2">
-              現在我々は、母なる星『地球』に住んでいますが、人類はどの時代でも宙を眺め観測してきました。
-              <br />
-              ですが我々が住む青い星地球は、さまざまな問題に直面している。
-              <br /><br />
-              母なる星、地球から独立し新たなるUniverse (宇宙)へ飛び出しましょう!
-            </div>
-          </div>
-        </div>
-      </a>
-
-      <a href='/photos'>
-        <div class="bg-violet-200 rounded-3xl px-12 py-8 mb-16 hover:opacity-70">
-          <div class="text-5xl font-bold">
-            写真ギャラリー
-          </div>
-          <div class="flex max-md:flex-col items-center justify-around mt-8">
-            <div class="max-md:w-full w-1/2">
-              <img src="/images/home/comingsoon.png" alt="" class="w-full rounded-lg" />
-            </div>
-            <div class="max-md:w-full w-1/2 font-bold text-3xl max-xl:text-2xl max-lg:text-xl max-md:text-lg ml-6 max-md:ml-0 border-l-4 max-md:border-l-0 border-black pl-6 max-md:px-3 max-md:mt-3 max-md:border-t-4 max-md:pt-2">
-              現在我々は、母なる星『地球』に住んでいますが、人類はどの時代でも宙を眺め観測してきました。
-              <br />
-              ですが我々が住む青い星地球は、さまざまな問題に直面している。
-              <br /><br />
-              母なる星、地球から独立し新たなるUniverse (宇宙)へ飛び出しましょう!
-            </div>
-          </div>
-        </div>
-      </a>
     </div>
   )
 }
