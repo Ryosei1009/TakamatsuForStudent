c196859f5b5a20ddef7f13bf420aa525f48409b3
ShinoChan0 <131277759+Ryosei1009@users.noreply.github.com>
2024-03-17 16:10:35 +0900

    誕生日リスト修正

--- frontend/src/components/eventCalendar/components/BirthdayList.js
+++ frontend/src/components/eventCalendar/components/BirthdayList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { fetchData } from '../../../utils/DatabaseUtil';
 import Loading from '../../_util/Loading';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const BirthdayList = () => {
     const [accounts, setAccounts] = useState([]);
@@ -13,40 +14,51 @@ const BirthdayList = () => {
         }, 500)
     }, []);
     return (
-        <div className="flex justify-center">
-            <table>
-                <thead>
-                    <tr>
-                        <th>名前</th>
-                        <th>誕生日</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {accounts.length > 0 ? (
-                        accounts.slice().map((item) => (
+        <>
+            <HelmetProvider>
+                <Helmet>
+                    <title>誕生日一覧 - TCFS</title>
+                </Helmet>
+            </HelmetProvider>
+            <div className="flex justify-center">
+                <table className="text-lg">
+                    <thead>
                         <tr>
-                            <th>
-                                {item.naming ? (item.naming) : (item.name)}
-                            </th>
-                            <th>
-                                {item.birthmonth + "月" + item.birthday + "日"}
-                            </th>
+                            <th className="px-2">名前</th>
+                            <th className="px-2">ニックネーム</th>
+                            <th className="px-2">誕生日</th>
                         </tr>
-                    ))
-                    ) : (
-                        <>
-                            {timer ? (
-                                <div className="text-3xl max-xl:text-2xl max-sm:text-lg font-bold ml-2 text-red-500">
-                                    サーバーが落ちている可能性があります。運営にお問い合わせください。
-                                </div>
-                            ) : (
-                                <Loading />
-                            )}
-                        </>
-                    )}
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        {accounts.length > 0 ? (
+                            accounts.slice().map((item) => (
+                                <tr>
+                                    <th className="px-3">
+                                        {item.name}
+                                    </th>
+                                    <th className="px-3">
+                                        {item.naming ? (item.naming) : ("")}
+                                    </th>
+                                    <th className="px-3">
+                                        {item.birthmonth + "月" + item.birthday + "日"}
+                                    </th>
+                                </tr>
+                            ))
+                        ) : (
+                            <>
+                                {timer ? (
+                                    <div className="text-3xl max-xl:text-2xl max-sm:text-lg font-bold ml-2 text-red-500">
+                                        サーバーが落ちている可能性があります。運営にお問い合わせください。
+                                    </div>
+                                ) : (
+                                    <Loading />
+                                )}
+                            </>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        </>
     )
 }
 
