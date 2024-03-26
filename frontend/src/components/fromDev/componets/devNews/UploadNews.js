bdf395be90e03cdb50c57b45315af857aa71e4ec
ShinoChan0 <131277759+Ryosei1009@users.noreply.github.com>
2024-03-26 23:51:30 +0900

    タイトル追加

--- frontend/src/components/fromDev/componets/devNews/UploadNews.js
+++ frontend/src/components/fromDev/componets/devNews/UploadNews.js
@@ -3,6 +3,7 @@ import { fetchData, postData } from '../../../../utils/DatabaseUtil';
 import { useAuth0 } from '@auth0/auth0-react';
 import Upload from './upload/Upload';
 import Preview from './upload/Preview';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const UploadDevNews = () => {
     const [eachAccount, setEachAccount] = useState({});
@@ -35,6 +36,13 @@ const UploadDevNews = () => {
 
     return (
         <>
+            <HelmetProvider>
+                <Helmet>
+                    <title>
+                        {uploadOpen ? "運営からのニュースを作成 - TCFS" : "運営から - TCFS"}
+                    </title>
+                </Helmet>
+            </HelmetProvider>
             <button onClick={() => setUploadOpen(true)} className="ml-2 text-white rounded-xl px-4 py-2 cursor-pointer font-bold bg-purple-600 hover:bg-purple-700">
                 ニュースを作成
             </button>
